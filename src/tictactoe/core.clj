4bd323998bc6a12be61829926595fab45fb6ef73
chris <cdresher2010@gmail.com>
2022-10-09 22:37:07 -0400

    finished funcs and testing of tictactoe.square

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,1 +1,13 @@
 (ns tictactoe.core)
+
+(defn check-player [player]
+  (case player
+    :x :x
+    :o :o
+    nil))
+
+
+(comment
+  (def test-board
+    )
+  )
--- src/tictactoe/square.clj
+++ src/tictactoe/square.clj
@@ -7,3 +7,21 @@
 (defn create-new-square [row col]
   (when (and (<= lower-bound row upper-bound) (<= lower-bound col upper-bound))
     {:row row :col col}))
+
+(defn create-squares []
+  (for [row (range upper-bound)
+        col (range upper-bound)]
+    {:row row :col col}))
+
+(defn create-board []
+  (into {} (for [square (create-squares)]
+             {square :empty})))
+
+(comment
+  (create-squares)
+  (def board (create-board))
+  (def board (assoc board {:row 0 :col 0} :x))
+  (def board (assoc board {:row 1 :col 0} :o))
+  (def board (assoc board {:row 2 :col 2} :x))
+  board
+  )
