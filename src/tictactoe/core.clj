9f92635cc013bd55d7c97d28fc6b42762da30e62
chris <cdresher2010@gmail.com>
2022-10-10 22:19:56 -0400

    completed place-piece and play-move. need to work on chaining funcs.

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,4 +1,5 @@
-(ns tictactoe.core)
+(ns tictactoe.core
+  (:require [tictactoe.square :as square]))
 
 (defn check-player [player]
   (case player
@@ -6,8 +7,20 @@
     :o :o
     nil))
 
+(defn place-piece [board location player]
+  (when (= (get board location) :empty)
+    (assoc board location player)))
+
+
+(defn play-move [board row col player]
+  (let [tile (square/create-new-square row col)]
+       (if-some [player (check-player player)]
+         player
+         :error)
+    (place-piece board tile player)))
+
 
 (comment
-  (def test-board
-    )
-  )
+  (def board (square/create-board))
+  (def board (play-move board 1 1 :x))
+  board)
