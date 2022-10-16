7e6fd2eee53ec0fb2e05cdcf3f1cb8200c02595c
chris <cdresher2010@gmail.com>
2022-10-16 17:52:44 -0400

    cleaned up core fns

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -3,14 +3,17 @@
 
 (defn check-player [player]
   (case player
-    :x :x
-    :o :o
+    :x player
+    :o player
     nil))
 
 (defn valid-location [board square]
-  (if (and (= (get board square) :empty) (some? square))
-   true
-   false))
+  (case (get board square)
+    :empty true
+    :x nil
+    :o nil
+    nil nil
+    :else nil))
 
 (defn place-piece [board location player]
   (if (valid-location board location)
@@ -21,9 +24,8 @@
 (defn play-move [board row col player]
   (let [tile (square/create-new-square row col)]
        (if-some [player (check-player player)]
-         player
-         :error)
-    (place-piece board tile player)))
+         (place-piece board tile player)
+         :invalid-player)))
 
 
 (comment
@@ -31,4 +33,6 @@
   (valid-location board (square/create-new-square 1 2))
   (def board (place-piece board (square/create-new-square 1 2) :x))
   (def board (play-move board 2 2 :o))
+  (def board (play-move board 2 3 :y))
+  (def board (play-move board 4 3 :x))
   board)
--- src/tictactoe/square.clj
+++ src/tictactoe/square.clj
@@ -2,15 +2,15 @@
 
 (def lower-bound 1)
 
-(def upper-bound 3)
+(def upper-bound 4)
 
 (defn create-new-square [row col]
-  (when (and (<= lower-bound row upper-bound) (<= lower-bound col upper-bound))
+  (when (and (<= lower-bound row (- upper-bound 1)) (<= lower-bound col (- upper-bound 1)))
     {:row row :col col}))
 
 (defn create-squares []
-  (for [row (range upper-bound)
-        col (range upper-bound)]
+  (for [row (range lower-bound upper-bound)
+        col (range lower-bound upper-bound)]
     {:row row :col col}))
 
 (defn create-board []
@@ -20,8 +20,8 @@
 (comment
   (create-squares)
   (def board (create-board))
-  (def board (assoc board {:row 0 :col 0} :x))
-  (def board (assoc board {:row 1 :col 0} :o))
-  (def board (assoc board {:row 2 :col 2} :x))
+  (def board (assoc board {:row 1 :col 1} :x))
+  (def board (assoc board {:row 2 :col 1} :o))
+  (def board (assoc board {:row 3 :col 3} :x))
   board
   )
